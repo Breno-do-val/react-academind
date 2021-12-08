afcdcad3e78383cafff2fe7582489be9acab15ae
bribeiro <bribeiro@daitan.com>
2021-12-08 18:06:25 -0300

    (feat): add ExpenseChart

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,6 +1,7 @@
 import React from 'react';
 
 import Card from '../UI/Card/Card';
+import ExpensesChart from './ExpensesChart/ExpensesChart';
 import ExpensesFilter from './ExpensesFilter/ExpensesFilter';
 import ExpensesList from './ExpensesList/ExpensesList';
 
@@ -9,18 +10,23 @@ import './Expenses.css';
 const Expenses = ({ expenses }) => {
   const [filtered, setFiltered] = React.useState('2020');
 
-  const filterChangeHandler = (selectedYear) => setFiltered(selectedYear);
+  const filterChangeHandler = (selectedYear) =>
+    setFiltered(selectedYear);
 
-  const filteredList = expenses.filter((expense) => new Date(expense.date).getFullYear().toString() === filtered);
+  const filteredList = expenses.filter(
+    (expense) =>
+      new Date(expense.date).getFullYear().toString() === filtered,
+  );
 
   return (
-      <Card className="expenses">
-        <ExpensesFilter
-          onFilterChange={filterChangeHandler}
-          selected={filtered}
-        />
-        <ExpensesList list={filteredList} />
-      </Card>
+    <Card className="expenses">
+      <ExpensesFilter
+        onFilterChange={filterChangeHandler}
+        selected={filtered}
+      />
+      <ExpensesChart expenses={filteredList} />
+      <ExpensesList list={filteredList} />
+    </Card>
   );
 };
 
