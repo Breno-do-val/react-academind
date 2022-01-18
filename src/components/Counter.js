e6a662475e37d534927b1054541dce83c65df4bd
bribeiro <bribeiro@daitan.com>
2022-01-18 18:34:42 -0300

    add new action

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -14,12 +14,19 @@ class Counter extends Component {
     this.props.decrement();
   };
 
+  handleIncrementByTen = () => {
+    this.props.incrementByTen();
+  };
+
   render() {
     return (
       <main className={classes.counter}>
         <h1>Redux Counter</h1>
         <div className={classes.value}>{this.props.counter}</div>
         <button onClick={this.handleIncrement.bind(this)}>Increment</button>
+        <button onClick={this.handleIncrementByTen.bind(this)}>
+          IncrementByTen
+        </button>
         <button onClick={this.handleDecrement.bind(this)}>Decrement</button>
         <button onClick={this.toggleCounterHandler}>Toggle Counter</button>
       </main>
@@ -36,6 +43,7 @@ const mapStateToProps = (state) => {
 const mapDispatchToProps = (dispatch) => {
   return {
     increment: () => dispatch({ type: "increment" }),
+    incrementByTen: () => dispatch({ type: "incrementByTen", payload: 10 }),
     decrement: () => dispatch({ type: "decrement" }),
   };
 };
