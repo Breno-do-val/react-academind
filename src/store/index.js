5310c12a4025dd8e2c2cf80b688829ed6d30ca5a
bribeiro <bribeiro@daitan.com>
2022-01-25 13:55:48 -0300

    refactor store

--- src/store/index.js
+++ src/store/index.js
@@ -1,53 +1,10 @@
-import { createSlice, configureStore } from "@reduxjs/toolkit";
+import { configureStore } from "@reduxjs/toolkit";
 
-const counterSlice = createSlice({
-  name: "counter",
-  initialState: {
-    counter: 0,
-    showCounter: true,
-  },
-  reducers: {
-    increment(state) {
-      state.counter++;
-    },
-    decrement(state) {
-      state.counter--;
-    },
-    incrementByTen(state, action) {
-      state.counter = state.counter + action.payload;
-    },
-    toggleCounterHandler(state) {
-      state.showCounter = !state.showCounter;
-    },
-  },
-});
-
-// Using redux only
-// const counterReducer = (state = { counter: 0 }, action) => {
-//   if (action.type === "increment") {
-//     return {
-//       counter: state.counter + 1,
-//     };
-//   }
-//   if (action.type === "incrementByTen") {
-//     return {
-//       counter: state.counter + action.payload,
-//     };
-//   }
-//   if (action.type === "decrement") {
-//     return {
-//       counter: state.counter - 1,
-//     };
-//   }
-//   return state;
-// };
+import counterReducer from "./counter";
+import authReducer from "./auth";
 
 const store = configureStore({
-  reducer: {
-    counter: counterSlice.reducer,
-  },
+  reducer: { counter: counterReducer, auth: authReducer },
 });
-
-export const counterActions = counterSlice.actions;
 
 export default store;
