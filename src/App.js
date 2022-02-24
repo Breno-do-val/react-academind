97ca68eadbccf95d5bc8b64117d86d6100cc7b3e
breno-do-val <brenorvale@gmail.com>
2022-02-23 23:50:40 -0300

    add dynamic routes

--- src/App.js
+++ src/App.js
@@ -1,15 +1,23 @@
 import { Routes, Route } from 'react-router-dom';
 
-import Products from './components/Products';
-import Welcome from './components/Welcome';
+import MainHeader from './components/MainHeader';
+
+import ProductDetail from './pages/ProductDetail';
+import Products from './pages/Products';
+import Welcome from './pages/Welcome';
 
 function App() {
   return (
     <div>
-      <Routes>
-        <Route path="/welcome" element={<Welcome />} />
-        <Route path="/products" element={<Products />} />
-      </Routes>
+      <MainHeader />
+      <main>
+        <Routes>
+          <Route path="welcome" element={<Welcome />} />
+          <Route index="products" element={<Products />}>
+            <Route path=":id" element={<ProductDetail />}/>
+          </Route>
+        </Routes>
+      </main>
     </div>
   );
 }
